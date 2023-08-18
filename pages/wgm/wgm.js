a4a40d4d054794c6aa2c4f4d6de447770774f301
王高敏 <w13545989373@163.com>
2023-08-18 14:02:18 +0800

    小程序使用及计数器功能

--- pages/wgm/wgm.js
+++ pages/wgm/wgm.js
@@ -5,9 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    msg:'hello world',
+    movies:['电影1','电影2', '电影3'],
+    counter: 0,
   },
 
+// 减少事件
+decrement(){
+    // 不同于vue写法，不单纯具备数据双向绑定，所以想要渲染修改后的数据要使用this.setData的方法
+
+    this.setData({
+        counter:this.data.counter-=1
+    })
+    // this.data.counter-=1;
+    // console.log('点击减少',this.data.counter);
+},
+// 增加事件
+increment() {
+    this.setData({
+        counter:this.data.counter+=1
+    })
+    // this.data.counter+=1;
+    // console.log('点击增加',this.data.counter);
+},
   /**
    * 生命周期函数--监听页面加载
    */
